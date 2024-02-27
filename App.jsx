6b104cb4afdac628bfa2963df99f1682ac5fd108
JulianaMArruda <139576858+JulianaMArruda@users.noreply.github.com>
2024-02-26 21:23:12 -0300

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,23 @@
+  import React from "react"
+  import Header from "./components/Header/Header.jsx"
+import Header from "./components/Header/Header.css"
+
+
+
+
+
+  function App() {
+  return (
+     <>
+    
+    <Header/>
+    <Header.css/>
+    
+    
+    </>
+    
+
+  )
+
+  }
+    export default App
---
+++ Header.jsx
@@ -0,0 +1,19 @@
+  import React from "react";
+   import Logo from "./logo/logo.jpg"
+
+
+
+  function Header (){
+    return(
+    <header>
+
+      <img src={Logo} alt="foto livros"/>
+    <h1>Livros da Ju </h1>
+
+
+    </header>
+    )
+  }
+
+
+  export default Header
